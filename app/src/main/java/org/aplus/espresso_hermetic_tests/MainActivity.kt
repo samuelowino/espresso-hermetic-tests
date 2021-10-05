94b261125af0226065608b35452c6625a8ad4cb9
samuelowino <samuelowino43@yahoo.com>
2021-10-05 10:56:06 +0300

    First espresso instrumented test

---
+++ app/src/main/java/org/aplus/espresso_hermetic_tests/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.aplus.espresso_hermetic_tests
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
